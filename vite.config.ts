59f563e17ec84101719d65f5ffbb025099e06d3c
dfd1123 <dfd1123@naver.com>
2022-02-28 17:51:06 +0900

    bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-import react from 'vite-preset-react';
+import react from '@vitejs/plugin-react';
+// import react from 'vite-preset-react';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
 const dotenv = require('dotenv');
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ removeDevtoolsInProd: true, injectReact: true }), VitePluginHtmlEnv()],
+  plugins: [react({
+    jsxRuntime: 'classic'
+  }), VitePluginHtmlEnv()],
   define: {
     'process.env': dotenv.config().parsed,
   },
