191f2c0f3d5f3d3d2df1e35465c67b5ad8c89437
Sungho Noh <nightwing@naver.com>
2022-03-23 13:01:04 +0900

    vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 import react from 'vite-preset-react';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
+
 const dotenv = require('dotenv');
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ removeDevtoolsInProd: true, injectReact: true }), VitePluginHtmlEnv()],
+  plugins: [
+    react({ removeDevtoolsInProd: true, injectReact: true }),
+    VitePluginHtmlEnv(),
+  ],
   define: {
     'process.env': dotenv.config().parsed,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
